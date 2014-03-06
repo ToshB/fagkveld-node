6b7f67ea70571123467d1eb4298812f9b3a697fe
Torstein Bjørnstad <tosh@tosh.no>
2014-03-06 17:44:19 +0100

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var responseHandler = require('./handler');
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.get('/', responseHandler.hello);
 app.get('/data', responseHandler.data);
